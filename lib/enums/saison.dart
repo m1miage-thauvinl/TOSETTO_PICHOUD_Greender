f931699227ef13a4d25d3e1f96affb45a5ef6d7c
Adrien PICHOUD <adrien.pichoud@cstb.fr>
2025-04-16 11:39:43 +0200

    Enum and class definitions for vegetables

---
+++ lib/enums/saison.dart
@@ -0,0 +1,10 @@
+enum Saison {
+  printemps ('Printemps'),
+  ete('Eté'),
+  automne('Automne'),
+  hiver('Hiver');
+
+  final String label;
+
+  const Saison(this.label);
+}
---
+++ lib/models/vegetable.dart
@@ -0,0 +1,8 @@
+import 'package:tosetto_pichoud_greender/enums/saison.dart';
+
+class Vegetable {
+  String name = '';
+  String description = '';
+  List<Saison> saisons = [];
+  String image = '';
+}
